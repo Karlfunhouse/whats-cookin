894a26603603c58d61d5b546c21840c6b42f2596
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-03-24 14:43:51 -0600

    :bar_chart: our first tests

--- test/User-test.js
+++ test/User-test.js
@@ -1,4 +1,25 @@
 const chai = require('chai');
 const expect = chai.expect;
+const User = require('../src/User');
+const usersData = require('../data/users');
+const recipeData = require('../data/recipes');
+const user1 = usersData[0];
 
-const User = require('../src/User.js')
+describe('User', function() {
+  let user;
+
+  beforeEach(function(){
+    user = new User(user1);
+    console.log(user)
+  });
+
+  it('user should be an instance of User', function(){
+    expect(user).to.be.an.instanceof(User)
+  });
+
+  it('should be able to favorite a recipe', function(){
+
+  });
+
+
+});
