45dae795be50caa8f903900784d83528d2178bf3
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-03-25 15:39:20 -0600

    :bar_chart: Added testing for filterByTag method

---
+++ test/Recipe-test.js
@@ -0,0 +1,38 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Recipe = require('../src/Recipe')
+const recipeData = require('../data/recipes');
+const ingredientsData = require('../data/ingredients');
+const recipe1 = recipeData[0];
+
+describe('Recipe', function() {
+  let recipe;
+
+  beforeEach(function(){
+    recipe = new Recipe(recipe1);
+  });
+
+  it('should be a an instance of Recipe', function(){
+    expect(recipe).to.be.an.instanceof(Recipe)
+  });
+
+  it('should be able to filter recipes by tag', function(){
+    expect(recipe.filterByTag('snack').length).to.equal(9)
+  });
+
+  // it('should be able to search recipes by ingredients', function(){
+  //
+  // });
+  //
+  // it('should be able to get the cost of its ingredients', function(){
+  //
+  // });
+  //
+  // it('should be able to get its instructions', function(){
+  //
+  // });
+
+
+
+
+});
