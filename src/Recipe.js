20a379b0c891b6072c27f76393fefbe78ccc7fce
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-03-25 15:39:01 -0600

    :crystal_ball: Added filterByTag method

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,8 +1,43 @@
 const recipeData = require('../data/recipes');
+const ingredientsData = require('../data/ingredients');
+
 
 class Recipe {
   constructor(recipeData) {
-    this.ingredients =
+    this.name = recipeData.name;
+    this.id = recipeData.id;
+    this.image = recipeData.image;
+    this.ingredients = recipeData.ingredients;
+    this.instructions = recipeData.instructions;
+    this.tags = recipeData.tags;
+  }
+
+  filterByTag(searchWord) {
+    //match ingredientsData.name to searchWord
+    //Match ingredientsData.id to recipeData.id
+    //Find
+      let lowerCaseSearchWord = searchWord.toLowerCase();
+
+      let foundRecipesByType =
+      recipeData.reduce((matchingRecipes, recipe) => {
+        recipe.tags.forEach(tag => {
+          if (tag === lowerCaseSearchWord) {
+            matchingRecipes.push(recipe);
+          }
+        });
+          return matchingRecipes;
+      }, []);
+
+      if (foundRecipesByType.length === 0) {
+        return 'We\'re Sorry, Your search did not return any results!'
+      } else {
+        return foundRecipesByType;
+      }
+    }
+
+
+  searchByIngredients(searchWord) {
+
   }
 
   getCostOfIngredients() {
