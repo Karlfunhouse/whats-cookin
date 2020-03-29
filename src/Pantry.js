c853fbe5d1f5fd30ecf0ed6e6911ffc0021b3982
John Watterson <john@breakthrewbrew.com>
2020-03-29 15:20:39 -0600

    :crystal_ball: Add method that locates a recipe's index

--- src/Pantry.js
+++ src/Pantry.js
@@ -9,9 +9,11 @@ class Pantry {
     this.usersPantry = user.pantry;
     this.ingredientsData = ingredientsData;
     this.recipeData = recipeData;
+    this.compiledPantryList = this.compileIngredientNamesQuantity();
   }
 
-  findIngredientNames() {
+  compileIngredientNamesQuantity() {
+    //compiles all data for usersPantry with useful data all in one location (array)
     return this.ingredientsData.reduce((ingredientList, currentIngredient) => {
       let matchingIngredients = this.usersPantry.filter(pantryIngredient => {
         pantryIngredient.ingredient === currentIngredient.id ? ingredientList.push(
@@ -26,9 +28,25 @@ class Pantry {
     }, []);
   }
 
-  checkIngredientSupply() {
-
+  locateRecipeIndex(id) {
+      let foundRecipe = this.recipeData.find(recipe => recipe.id === id);
+      return recipeData.indexOf(foundRecipe);
   }
+
+  checkIngredientSupply(recipeId) {
+    let recipe = recipeData[this.locateRecipeIndex(recipeId)];
+    //compare recipe.ingredients to this.compileIngredientNamesQuantity;
+
+    //return any ingredient in compiled list that is less than ingredient
+    //required for recipe + the difference to have correct amount of ingredient;
+    let recipeIngredients = recipe.ingredients;
+    let pantry = this.compiledPantryList;
+
+    recipeIngredients.reduce((ingredientsNeeded, currentIngredient) => {
+      return ingredientsNeeded
+    }, []);
+  }
+
   //determine if i have enough ingredients in Pantry
   //to cook meal
 
