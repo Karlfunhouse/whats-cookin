2d1ce6c55ab64d6197bd2635d48f655eef7ed75b
John Watterson <john@breakthrewbrew.com>
2020-03-31 16:19:38 -0600

    :crystal_ball: Hooked up all queried buttons to run in global event handler :crystal_ball:

--- src/scripts.js
+++ src/scripts.js
@@ -14,12 +14,22 @@ let searchBar = document.querySelector('#search-bar')
 let searchButton = document.querySelector('.search-button-js')
 let featuredRecipeSection = document.querySelector('.featured-recipe')
 
-searchButton.addEventListener('click', globalEventHandler)
+allRecipes.addEventListener('click', globalEventHandler);
+myRecipes.addEventListener('click', globalEventHandler);
+recipesToCook.addEventListener('click', globalEventHandler);
+searchButton.addEventListener('click', globalEventHandler);
+
 populateFeaturedRecipe()
 function globalEventHandler(event) {
   event.preventDefault();
-  if(event.target.classList.contains('search-button-js')) {
-    console.log('fucking christ')
+  if(event.target === searchButton) {
+    console.log('search button hit')
+  } else if (event.target === allRecipes) {
+    console.log('all recipes hit');
+  } else if (event.target === myRecipes) {
+    console.log('my recipes button hit');
+  } else if (event.target === recipesToCook) {
+    console.log('recipe to cook button hit');
   }
 }
 
@@ -33,7 +43,8 @@ function populateFeaturedRecipe() {
   let featuredRecipe = new Recipe(randomRecipe)
   featuredRecipeSection.insertAdjacentHTML('afterbegin', `<img src=${featuredRecipe.image} alt="" class="featured-recipe-photo">
   <div class="featured-recipe-text">
-    <h2>FEATURED RECIPE: </br>${featuredRecipe.name}</h2>
+    <h2><span class="featured-recipe-title">FEATURED RECIPE</span></h2></br>
+    <h3>${featuredRecipe.name}<h3>
     <hr>
     <h4>Cost: ${featuredRecipe.getCostOfIngredients()}</h4>
     <h4>Ingredients:</h4>
