8420cb856a2689d75163161ca8a53d7482cb3f7b
John Watterson <john@breakthrewbrew.com>
2020-03-31 17:19:56 -0600

    :crystal_ball: Add functionality to clear section on button click

--- src/scripts.js
+++ src/scripts.js
@@ -1,22 +1,19 @@
-// const recipeData = require('../data/recipes');
-// const ingredientsData = require('../data/ingredients');
-// const userData = require('../data/users')
-// let randomNumber = ((Math.ceil(Math.random())));
 let users = usersData;
-let recipes = recipeData;
+let allRecipes = recipeData;
 let ingredients = ingredientsData;
-let user = new User(generateRandom(users))
-console.log(user);
-let allRecipes = document.querySelector('.all-recipe-button-js');
-let myRecipes = document.querySelector('.my-recipes-button-js');
-let recipesToCook = document.querySelector('.recipes-to-cook-button-js');
-let searchBar = document.querySelector('#search-bar')
-let searchButton = document.querySelector('.search-button-js')
-let featuredRecipeSection = document.querySelector('.featured-recipe')
+let user = new User(generateRandom(users), allRecipes, ingredients);
+// console.log(user);
+let allRecipesBtn = document.querySelector('.all-recipe-button-js');
+let myRecipesBtn = document.querySelector('.my-recipes-button-js');
+let recipesToCookBtn = document.querySelector('.recipes-to-cook-button-js');
+let searchBar = document.querySelector('#search-bar');
+let searchButton = document.querySelector('.search-button-js');
+let featuredRecipeSection = document.querySelector('.featured-recipe-js');
+let allRecipesSection = document.querySelector('.all-recipe-cards-js');
 
-allRecipes.addEventListener('click', globalEventHandler);
-myRecipes.addEventListener('click', globalEventHandler);
-recipesToCook.addEventListener('click', globalEventHandler);
+allRecipesBtn.addEventListener('click', globalEventHandler);
+myRecipesBtn.addEventListener('click', globalEventHandler);
+recipesToCookBtn.addEventListener('click', globalEventHandler);
 searchButton.addEventListener('click', globalEventHandler);
 
 populateFeaturedRecipe()
@@ -24,11 +21,13 @@ function globalEventHandler(event) {
   event.preventDefault();
   if(event.target === searchButton) {
     console.log('search button hit')
-  } else if (event.target === allRecipes) {
+  } else if (event.target === allRecipesBtn) {
+    removeTargetedSection(featuredRecipeSection);
+    populateRecipeCards(allRecipes);
     console.log('all recipes hit');
-  } else if (event.target === myRecipes) {
+  } else if (event.target === myRecipesBtn) {
     console.log('my recipes button hit');
-  } else if (event.target === recipesToCook) {
+  } else if (event.target === recipesToCookBtn) {
     console.log('recipe to cook button hit');
   }
 }
@@ -39,8 +38,8 @@ function generateRandom(data) {
 }
 
 function populateFeaturedRecipe() {
-  let randomRecipe = generateRandom(recipes)
-  let featuredRecipe = new Recipe(randomRecipe)
+  let randomRecipe = generateRandom(allRecipes);
+  let featuredRecipe = new Recipe(randomRecipe, ingredients);
   featuredRecipeSection.insertAdjacentHTML('afterbegin', `<img src=${featuredRecipe.image} alt="" class="featured-recipe-photo">
   <div class="featured-recipe-text">
     <h2><span class="featured-recipe-title">FEATURED RECIPE</span></h2></br>
@@ -50,6 +49,11 @@ function populateFeaturedRecipe() {
     <h4>Ingredients:</h4>
   </div>`);
 }
+
+function removeTargetedSection(sectionToTarget) {
+  sectionToTarget.classList.add('hidden');
+}
+
 
 function populateRecipeCards(recipes) {
 
