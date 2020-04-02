d8556a1299c3752f7543a63e30aa8007c0fbffec
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-04-02 07:51:35 -0600

    :bar_chart: Fixed Data Flow based on updated methods

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -3,34 +3,33 @@ const expect = chai.expect;
 const Recipe = require('../src/Recipe')
 const recipeData = require('../data/recipes');
 const ingredientsData = require('../data/ingredients');
-const recipes = recipeData;
+const recipe1 = recipeData[0];
 
 describe('Recipe', function() {
   let recipe;
 
   beforeEach(function(){
-    allRecipe = new Recipe(recipes, ingredientsData);
-    recipe1 = new Recipe(recipes[0], ingredientsData)
+    recipe = new Recipe(recipe1, ingredientsData);
   });
 
   it('should be a an instance of Recipe', function(){
-    expect(recipe1).to.be.an.instanceof(Recipe)
+    expect(recipe).to.be.an.instanceof(Recipe)
   });
 
   it('should be able to filter recipes by tag', function(){
-    expect(allRecipe.filterByTag('snack').length).to.equal(9)
+    expect(recipe.filterByTag('snack', recipeData).length).to.equal(9)
   });
 
   it('should be able to search recipes by ingredients', function(){
-    expect(allRecipe.searchByIngredient('brown sugar').length).to.equal(12)
+    expect(recipe.searchByIngredient('brown sugar', recipeData).length).to.equal(12)
   });
 
   it('should be able to get the cost of its ingredients', function(){
-    expect(recipe1.getCostOfIngredients()).to.equal('$177.76')
+    expect(recipe.getCostOfIngredients(ingredientsData)).to.equal('$177.76')
   });
 
   it('should be able to get its instructions', function(){
-    expect(recipe1.getRecipeInstructions().length).to.deep.equal(6)
+    expect(recipe.getRecipeInstructions().length).to.deep.equal(6)
   });
 
 
